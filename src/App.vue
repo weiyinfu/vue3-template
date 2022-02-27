87f53f5b6cc7d9c58139bc35d391ff2f73bc9f01
weiyinfu.weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2022-02-27 20:43:01 +0800

    good

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-view/>
+</template>
+<script>
+    export default {
+        mounted() {
+        }
+    }
+</script>
+<style>
+    html, body, #app {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Home">
+    hello
+  </div>
+</template>
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  component: {},
+  mounted() {
+  }
+}
+</script>
+
+<style lang="less">
+.Home {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  .group {
+    border: 1px solid;
+    padding: 13px;
+    margin: 4px;
+    display: inline-flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
